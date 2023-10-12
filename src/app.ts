99359e1ee270aee85c7302aedefbb149ba75e557
Christian Bromann <git@bromann.dev>
2023-10-12 09:18:28 -0700

    fix windows build

--- src/app.ts
+++ src/app.ts
@@ -52,8 +52,10 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// set correct icon during development
-app.dock.setIcon(path.join(__dirname, 'assets', 'icon', 'webdriverio.png'))
+// set correct icon during development on macOS
+if (process.platform === 'darwin') {
+  app.dock.setIcon(path.join(__dirname, 'assets', 'icon', 'webdriverio.png'))
+}
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
